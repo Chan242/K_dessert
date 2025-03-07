0843fa791cebfd258dfa04d9f9cf43f0d6192400
ogremage01 <curselife01@gmail.com>
2025-03-07 17:17:19 +0900

    서블릿 경로 변경

--- webProject/Kdessert/src/main/java/board/FreeBoardReplyController.java
+++ webProject/Kdessert/src/main/java/board/FreeBoardReplyController.java
@@ -19,7 +19,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebServlet("/board/write")
+@WebServlet("/board/write/reply")
 public class FreeBoardReplyController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
