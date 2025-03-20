c36fc93af32f28ffe8b188635dbb6d99230452b0
ogremage01 <curselife01@gmail.com>
2025-03-20 11:06:57 +0900

    첫 버튼 연결

---
+++ webProject/Kdessert/src/main/java/user/dessert/controller/DessertDetailController.java
@@ -0,0 +1,44 @@
+package user.dessert.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class DessertDetailController
+ */
+@WebServlet("/dessert/dagwasogaejasehibogi")
+public class DessertDetailController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DessertDetailController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher rd = req.getRequestDispatcher("/page/member/dessert/dagwasogaejasehibogi.jsp");
+		rd.forward(req, res);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, res);
+	}
+
+}
